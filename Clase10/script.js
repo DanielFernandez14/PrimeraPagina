991d3437effe62b21ef958ed50cb5d444a507803
Daniel Matias Fernandez <daniel.mfernandez1999@gmail.com>
2024-01-15 18:59:41 -0300

    first commit

---
+++ Clase10/script.js
@@ -0,0 +1,50 @@
+/* todo el comportamiento el sitio (acciones que podemos hacer con el sitio)
+- manejo de datos- informacion
+- tipo de datos/valores interpreta JS*/
+/*String (caracteres); numbers (numeros); booleans (boleanos) */
+"Juan"//string todo lo que se encuentre englobado entre comillas lo toma caracteres es un tipo de datos
+
+123
+// 123 reconoce los numeros, decimales con . 
+
+// booleans 
+//valores verdaderos y falsos -> palabras asignadas por JS
+true 
+false
+//se ponen en minuscula y en ingles
+
+//almacenar informacion ->variables (espacio de memoria donde guardar info que 
+// utilizamos a lo largo de todo el documento js)
+
+//crear variables -> (let - cont - var)
+
+var persona="jose"; //no muy recomendable porque es poco estrica, muy permisiva y da lugar al error humano
+
+let otrapersona="marta"; //evitar que creemos mas de un espacio de memoria con el mismo nombre
+//permite cambiar el contenido si queremos
+//let no me permite crear mas de una variable con el mismo nombre
+
+const numero=123123 //const evita crear mas de un espacio de memoria con el mismo nombre
+//impide cambiar su contenido una vez creado
+
+
+/* //pal nombre= valor; */
+
+//utilizar variables
+//"llamar" a la variable, nombrarla
+
+
+//modificar variables
+//mostrar por consola --> browser
+
+//operaciones -> aritmeticas - logicas - de comparación
+/* las operaciones estan ligadas a los operadores, que son aquellos que nos 
+permiten tomar dos vcalores y devolver un tercero. nos permiten obtener una informacion
+a partir de dos valores */
+
+//Aritmeticas -> calculos matematicos: suma, resta, multiplicación. Reconoce los simbolos.
+// console.log(10+33)
+//let resultado=300-11; //calculo matematico ente dos numeros y guardar dicho 
+//resultado en la variable
+/* */
+
---
+++ Clase14/js.js
@@ -0,0 +1,15 @@
+const curso={
+    nombreCurso:"PWA",
+    cantidadDeAlumnos:34,
+    nombreDocente:"Juan Perez",
+    fechadeinicio:"01/01/24"
+};
+
+let listaDeCursos={
+    {
+        nombreCurso:"PWA",
+        cantidadDeAlumnos:44,
+        nombreDocente:"Juan Perez",
+        fechadeinicio :"01/02/24"
+    }
+};
